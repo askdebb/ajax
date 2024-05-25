b2a281f5938197aa688381b70713f2b96c54a324
Christopher Wiafe Debrah <cd.jobs.w@gmail.com>
2024-05-25 14:51:50 +0000

    fetch data from external api using ajax

--- js-ajax-request/script.js
+++ js-ajax-request/script.js
@@ -60,3 +60,44 @@ let displayJSONData = (jsonObject) => {
 
     document.getElementById('json-card').innerHTML = jsonTemplate;
 }
+
+// API Button
+let apiButton = document.getElementById('api-btn');
+
+apiButton.addEventListener('click', () => {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/users', true);
+    xhr.send();
+    xhr.onload = () => {
+        if(xhr.status === 200) {
+            let apiJSONData = xhr.responseText;
+            let apiJSONObject = JSON.parse(apiJSONData)
+            displayUsers(apiJSONObject);
+        }
+    }
+});
+
+// display JSON
+
+let displayUsers = (apiJSONObject) => {
+    let apiHtmlTemplate = '';
+    for(let user of apiJSONObject) {
+        apiHtmlTemplate += `<ul class="list-group mb-3">
+                                <li class="list-group-item"><b>ID:</b> ${user.id}</li>
+                                <li class="list-group-item"><b>NAME:</b> ${user.name}</li>
+                                <li class="list-group-item"><b>USERNAME:</b> ${user.username}</li>
+                                <li class="list-group-item"><b>STREET:</b> ${user.address.street}</li>
+                                <li class="list-group-item"><b>SUITE:</b> ${user.address.suite}</li>
+                                <li class="list-group-item"><b>CITY:</b> ${user.address.city}</li>
+                                <li class="list-group-item"><b>ZIP CODE:</b> ${user.address.zipcode}</li>
+                                <li class="list-group-item"><b>COMPANY NAME:</b> ${user.company.name}</li>
+                                <li class="list-group-item"><b>COMPANY CATCHPHRASE:</b> ${user.company.catchPhrase}</li>
+                                <li class="list-group-item"><b>COMPANY BS:</b> ${user.company.bs}</li>
+                                <li class="list-group-item"><b>EMAIL:</b> ${user.email}</li>
+                                <li class="list-group-item"><b>PHONE:</b> ${user.phone}</li>
+                                <li class="list-group-item"><b>WEBSITE:</b> ${user.website}</li>
+                            </ul> `;
+
+        document.getElementById('api-card').innerHTML = apiHtmlTemplate;
+    }
+};
